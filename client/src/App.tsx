cd14f754eb1e973206053e32f831238a53cfc8e0
ben <wbpiggot@gmail.com>
2019-01-14 22:14:39 -0800

    setting up basic apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql',
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
+
+class App extends React.Component<{}, {}> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ApolloProvider>
     );
   }
 }
