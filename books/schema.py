8d60e33d8ccdfd54a5db05336e9bd10c559ac0b9
ben <wbpiggot@gmail.com>
2019-01-14 17:46:12 -0800

    Adding single item queries

--- books/schema.py
+++ books/schema.py
@@ -3,22 +3,26 @@ import graphene
 
 from .models import Book, Review, PurchaseVenue
 
-class BookNode(DjangoObjectType):
+class BookType(DjangoObjectType):
     class Meta:
         model = Book
 
-class ReviewNode(DjangoObjectType):
+class ReviewType(DjangoObjectType):
     class Meta:
         model = Review
 
-class PurchaseVenueNode(DjangoObjectType):
+class PurchaseVenueType(DjangoObjectType):
     class Meta:
         model = PurchaseVenue
 
 class Query(graphene.ObjectType):
-    books = graphene.List(BookNode)
-    reviews = graphene.List(ReviewNode)
-    purchase_venues = graphene.List(PurchaseVenueNode)
+    books = graphene.List(BookType)
+    reviews = graphene.List(ReviewType)
+    purchase_venues = graphene.List(PurchaseVenueType)
+    
+    book = graphene.Field(BookType,id=graphene.String())
+    review = graphene.Field(ReviewType, id=graphene.String())
+    purchase_venue = graphene.Field(PurchaseVenueType, id=graphene.String())
 
     def resolve_books(self, info):
         return Book.objects.all()
@@ -28,5 +32,24 @@ class Query(graphene.ObjectType):
 
     def resolve_purchase_venues(self, info):
         return PurchaseVenue.objects.all()
+
+    def resolve_book(self, info, **args):
+        id = args.get('id')
+        if id is not None:
+          return Book.objects.get(pk=id)
+        return None
+
+    def resolve_reivew(self, info, **args):
+        id = args.get('id')
+        if id is not None:
+          return Review.objects.get(pk=id)
+        return None
+
+    def resolve_purchase_venue(self, info, **args):
+        id = args.get('id')
+        if id is not None:
+          return PurchaseVenue.objects.get(pk=id)
+        return None
+        
 
 schema = graphene.Schema(query=Query)
